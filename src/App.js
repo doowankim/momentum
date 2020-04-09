a978effc5dc06b5e0b68725cea9f698c90bee2bb
Doowankim <rlaendhks11@gmail.com>
2020-04-09 09:41:36 +0900

    initial setting

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import GlobalStyle from "./components/GlobalStyle";
+
+function App() {
+  return (
+    <div className="App">
+      <GlobalStyle />
+      모멘텀 만들기
+    </div>
+  );
+}
+
+export default App;
